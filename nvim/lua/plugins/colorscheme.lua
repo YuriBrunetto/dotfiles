b443d08e881241eae0742eb07bb8cd7d9872a4d5
Yuri Brunetto <brunettoyuri@gmail.com>
2024-04-06 09:41:01 -0300

    enable gruvbox transparent

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,16 @@
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      transparent_mode = true,
+    },
+  },
 
   -- configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      background = "dark",
       colorscheme = "gruvbox",
     },
   },
